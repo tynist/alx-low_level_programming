925cb266e5e7c295b69a031e35c9a96d303b6577
Ughasi Peter <vmailvp@gmail.com>
2022-07-07 14:39:28 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * print_numbers - prints numbers between 0 to 9.
+ *
+ *   * Return: no return.
+ *
+ *    */
+
+void print_numbers(void)
+
+{
+
+		int numbers;
+
+
+
+			for (numbers = 48; numbers <= 58; numbers++)
+
+					{
+
+								_putchar(numbers);
+
+									}
+
+				_putchar('\n');
+
+}
