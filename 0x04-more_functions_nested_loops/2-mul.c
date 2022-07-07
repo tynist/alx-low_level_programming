25c26374f78873d2900d902ceba594caf4591788
Ughasi Peter <vmailvp@gmail.com>
2022-07-07 14:20:39 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * mul - gives the product of a and b
+ *
+ *   * @a: variable
+ *
+ *    * @b: vari:wq
+ *    :wqsdfsdfsdf
+ *    iuyuyiuqwyi
+ *    :wq
+ *
+ *
+ *
+ *
+*    icvashdsjjfjj
