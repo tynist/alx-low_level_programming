a3ab25af7c1660bc1ad8a1631632031e3b7a8051
Peter <86600697+tynist@users.noreply.github.com>
2022-07-12 14:41:24 +0100

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,11 +11,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; *(src + i) != '\0'; i++)
 	{
-		dest[i] = src[1];
+		dest[i] = *(src + i);
 	}
-	dest[i++] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
