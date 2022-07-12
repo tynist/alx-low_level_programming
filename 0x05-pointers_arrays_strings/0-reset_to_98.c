67f51aa274e26def7378a3de9fea66b11143d303
Ughasi Peter <vmailvp@gmail.com>
2022-07-12 03:15:27 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 /**
- * * reset_to_98 - takes a pointer and updates the value to
- * * @n: pointer
- * * Return : 0
- * */
+* reset_to_98 - takes a pointer and updates the value to
+* @n: pointer
+* Return : 0
+*/
 
 void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
