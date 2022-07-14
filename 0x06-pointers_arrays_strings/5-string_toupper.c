751af343ff0a13e1be3651252bf81ec9d804fad0
Ughasi Peter <vmailvp@gmail.com>
2022-07-14 06:49:04 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * string_toupper - changes all lowercase letters of a string to uppercase
+ *
+ *   * @str: string to be changed
+ *
+ *    * Return: pointer to changed
