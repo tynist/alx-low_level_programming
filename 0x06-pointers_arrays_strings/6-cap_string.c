05edf262bc78cfeaf5e37dad8e0225291c817eac
Ughasi Peter <vmailvp@gmail.com>
2022-07-14 09:20:28 -0700

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+/**
+ * cap_string - function that capitalize words of a string
+ * @str: string to capitalize
+ * Return: capitalized string
+ */
+
+char *cap_string(char *str)
+{
+	char spc[] = {32, 9, '\n', ',', ';', '.', ',', '"', '(', '{', '}'};
+
+	int len = 13; int a = 0, i;
+	while (str[a])
+	{
+		i = 0;
+		while (i < len)
+		{
+			if ((a == 0 || str[a - 1] == spc[i]) && (str[a] >= 97 && str[a] <= 122))
+				str[a] = str[a] - 32;
+			i++;
+		}
+		a++;
+	}
+	return (str);
+}
