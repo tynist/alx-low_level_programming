866fe1617cddfb32d9041e22cbae49e03cb785d8
Peter <86600697+tynist@users.noreply.github.com>
2022-07-14 18:49:27 +0100

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: string to ciphered
+ * Return: ciphered string
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char cipher[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; alphabet[j] != '\0'; j++)
+		{
+			if (str[i] == alphabet[j])
+			{
+				str[i] = cipher[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
