4b0c7ecda1015ff85a3bc9372fe284002e12757f
Peter <86600697+tynist@users.noreply.github.com>
2022-07-14 19:16:23 +0100

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,29 +7,16 @@
 
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		m = n * -1;
-	}
-	else
-	{
-		m = n;
+		_putchar('-');
+		num = -num;
 	}
 
-	d = m;
-	count = 1;
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+	_putchar((num % 10) + '0');
 }
