15e3f74b0fc4953a8b36bb3f051c8c05c6a33001
Peter <86600697+tynist@users.noreply.github.com>
2022-07-20 21:39:56 +0100

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,38 +1,28 @@
 #include "main.h"
-/**
- * sqtChecker - computes square root recursively doing binary search
- * @min: inital number
- * @max: last number within the limit of number
- * @m: given number
- * Return: 1 if not found sqrroot, else sqrroot
- **/
-int sqtChecker(int min, int max, int m)
-{
-	long int guess;
 
-	if (max >= min)
-	{
-		guess = min + (max - min) / 2;
-		if (guess * guess == m)
-			return (guess);
-/* following binary search */
-		if (guess * guess > m)
-			return (sqtChecker(min, guess - 1, m));
-		if (guess * guess < m)
-			return (sqtChecker(guess + 1, max, m));
-	}
-	return (-1);
-}
 /**
- * _sqrt_recursion - finds the natural square root of a number
- * @n: given number
- * Return: square root of n or -1
- **/
-	int _sqrt_recursion(int n)
+ * _sqrt_recursion - main funct
+ * @n: int n
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt - _sqrt_recursion
+ * @n: integer paramtr
+ * @i: integer parameter
+ * Return: sqrt
+ */
+int _sqrt(int n, int i)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 0)
-		return (0);
-	return (sqtChecker(1, n, n));
+	if ((i * i) > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + 1));
 }
