aa49532b6e3d2f81b0bde8da0a9a53bbc067141d
Ughasi Peter <vmailvp@gmail.com>
2022-07-20 08:32:51 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,15 +3,18 @@
 /**
  * _puts_recursion - prints a string, followed by a new line
  * @s: string to be printed
+ * Return: 
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	else
+	{
+	_putchar('\n');
+	}
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,7 +5,7 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 void _putchar(char c);
 int _strlen_recursion(char *s);
-iendifnt factorial(int n);
+int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt(int n, int i);
