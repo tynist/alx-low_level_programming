cb0f34c29d29ef743c2bbfc39159c62879f7d928
Peter <86600697+tynist@users.noreply.github.com>
2022-07-20 16:37:29 +0100

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,18 +3,16 @@
 /**
  * _puts_recursion - prints a string, followed by a new line
  * @s: string to be printed
- * Return: 
+ * Return: 0
  */
 
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_putchar(*s);
+		putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else
-	{
-	_putchar('\n');
-	}
+		putchar('\n');
 }
