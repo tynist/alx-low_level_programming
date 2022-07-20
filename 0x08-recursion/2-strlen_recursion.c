06a618a873194a9f8e43cc74ac3b25035444384b
Peter <86600697+tynist@users.noreply.github.com>
2022-07-20 18:12:41 +0100

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen_recursion - returns the length of a string
- * @s: pointer to 
+ * @s: pointer to
  * Return: recursion
  */
 
@@ -10,5 +10,5 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	return (1 + _strlen_recursion(s + 1));
+	return (1 + _strlen_recursion(++s));
 }
