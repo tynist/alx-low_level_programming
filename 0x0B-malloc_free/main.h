99c2885f21d62ca896ef0aa773a8ffb5e53dd4e4
Ughasi Peter <vmailvp@gmail.com>
2022-07-25 06:25:27 -0700

    Task 0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,11 +1,14 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
+void free_grid(int **grid, int height);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
+int **alloc_grid(int width, int height);
+int len(char *str);
+int num_words(char *str);
 
 #endif
