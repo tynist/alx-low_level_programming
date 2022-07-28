9b2b08fd2b7afbfce434f654b6d5dc32d5d877ae
Peter <86600697+tynist@users.noreply.github.com>
2022-07-28 17:18:29 +0100

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,39 +1,97 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - multiplies two positive numbers
- * @argc: numbers of arguments
- * @argv: array of argument
- * Return: 0 (Success)
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ * Return: 0
  */
 
-int main(int argc, char **argv)
+int is_digit(char *s)
 {
-	int i, j;
-	unsigned long num1, num2;
+	int i = 0;
 
-	if (argc != 3)
+	while (s[i])
 	{
-		printf("Error\n");
-		exit(98);
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
 	}
+	return (1);
+}
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: pointer
+ */
+int _strlen(char *s)
+{
+	int i = 0;
 
-	for (i = 1; i < argc; i++)
+	while (s[i] != '\0')
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		i++;
+	}
+	return (i);
+}
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+
+	result = malloc(sizeof(int) * len);
+
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			if (argv[i][j] < 48 || argv[i][j] > 57)
-			{
-				printf("Error\n");
-				exit(98);
-			}
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-
-	num1 = atol(argv[1]);
-	num2 = atol(argv[2]);
-
-	printf("%lu\n", num1 * num2);
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
