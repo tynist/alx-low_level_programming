e0f19b4658f17fe9c6311328f15d7666df50477a
Ughasi Peter <vmailvp@gmail.com>
2022-07-28 05:32:01 -0700

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,15 +4,15 @@
 /**
  * malloc_checked - allocates memory using malloc
  * @b: size of the memory
- * Return: pointer to the address of the memory 
+ * Return: pointer to the address of the memory
  */
 
 void *malloc_checked(unsigned int b)
 {
-	void *a;
+	void *mem;
 
-	a = malloc(b);
-	if (a == NULL)
+	mem = malloc(b);
+	if (mem == NULL)
 		exit(98);
-	return (a);
+	return (mem);
 }
