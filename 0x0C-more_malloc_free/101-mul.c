c8c97bd2f1e8cd0af16c2a70a2a4d5f8c25fd594
Ughasi Peter <vmailvp@gmail.com>
2022-07-28 12:41:44 -0700

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,29 +1,31 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - multiplies two positive numbers
  * @argc: n arguments
  * @argv: args
- * Return: int
+ * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
-unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
+	if (argc <= 2 || argc > 3)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
-
+		printf("%s\n", "Error");
+			return (98);
 	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
-return (0);
+	else if ((atoi(argv[argc - 1]) * atoi(argv[argc - 2])) > 0)
+	{
+		printf("%d\n", (atoi(argv[argc - 1]) * atoi(argv[argc - 2])));
+		return (0);
+	}
+	if ((*argv[argc - 1] == '0') ||  (*argv[argc - 2]) == '0')
+	{
+		printf("%d\n", (atoi(argv[argc - 1]) * atoi(argv[argc - 2])));
+		return (0);
+	}
+	printf("%s\n", "Error");
+	return (98);
 }
