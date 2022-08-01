069208ff72ffcd134f3be4f4ee6d2581a50fcf45
Ughasi Peter <vmailvp@gmail.com>
2022-08-01 14:18:22 -0700

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file of the program
+ *        was compiled from
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
