3fd03128c0d64e7b69d98bc3157b1f4a3eb302ad
Ughasi Peter <vmailvp@gmail.com>
2022-08-03 03:59:55 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: pointer to a function that prints a name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
