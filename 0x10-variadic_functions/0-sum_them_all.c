0aa7e58b73826050313cdd11544e7802b4092504
Peter <86600697+tynist@users.noreply.github.com>
2022-08-04 13:04:39 +0100

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,10 +4,8 @@
 /**
  * sum_them_all - returns the sum of all its parameters
  * @n: amount of the arguments
- * Return: 0 if n = Null
- * 	otherwise sum of its parameters
+ * Return: sum of its parameters
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list nums;
