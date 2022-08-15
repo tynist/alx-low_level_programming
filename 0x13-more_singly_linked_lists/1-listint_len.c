b4a626bf3f0f56f74229db0380fc53c0f8af9969
Peter <86600697+tynist@users.noreply.github.com>
2022-08-15 19:51:58 +0100

    listint_len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - returns the number of elements in a linked list
+ * listint_len - returns the number of elements in a linked list
  * @h: pointer to the head of a list
  *
  * Return: numbers of elements
