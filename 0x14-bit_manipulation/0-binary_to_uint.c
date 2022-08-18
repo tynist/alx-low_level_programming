492eb35480df103840bac0113f4779611f52ceef
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-08-18 13:22:04 +0100

    binary_to_unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,20 +6,38 @@
  * Return: If b is NULL or contains chars not 0 or 1 - 0
  *         Otherwise - the converted number
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int len = 0;
+	unsigned int ui;
+	int len, base_two;
 
-	if (b[len] == '\0')
+	if (!b)
 		return (0);
 
-	while ((b[len] == '0') || (b[len] == '1'))
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		num <<= 1;
-		num += b[len] - '0';
-		len++;
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
 
-	return (num);
+	return (ui);
 }
+
+}
+}
+
+}
+
