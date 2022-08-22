b8b963eeb8a5568ab0267b35f60a9f25444c609e
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-08-22 20:42:57 +0100

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,20 +21,20 @@ void check97(int argc)
  * check98 - checks that file_from exists and can be read
  * @check: checks if true of false
  * @file: file_from name
- * @op_from: file descriptor of file_from, or -1
- * @op_to: file descriptor of file_to, or -1
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
  *
  * Return: void
  */
-void check98(ssize_t check, char *file, int op_from, int op_to)
+void check98(ssize_t check, char *file, int fd_from, int fd_to)
 {
 	if (check == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
-		if (op_from != -1)
-			close(op_from);
-		if (op_to != -1)
-			close(op_to);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
 		exit(98);
 	}
 }
@@ -43,20 +43,20 @@ void check98(ssize_t check, char *file, int op_from, int op_to)
  * check99 - checks that file_to was created and/or can be written to
  * @check: checks if true of false
  * @file: file_to name
- * @op_from: file descriptor of file_from, or -1
- * @op_to: file descriptor of file_to, or -1
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
  *
  * Return: void
  */
-void check99(ssize_t check, char *file, int op_from, int op_to)
+void check99(ssize_t check, char *file, int fd_from, int fd_to)
 {
 	if (check == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-		if (op_from != -1)
-			close(op_from);
-		if (op_to != -1)
-			close(op_to);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
 		exit(99);
 	}
 }
@@ -64,15 +64,15 @@ void check99(ssize_t check, char *file, int op_from, int op_to)
 /**
  * check100 - checks that file descriptors were closed properly
  * @check: checks if true or false
- * @fop: file descriptor
+ * @fd: file descriptor
  *
  * Return: void
  */
-void check100(int check, int op)
+void check100(int check, int fd)
 {
 	if (check == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close op %d\n", op);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
@@ -85,30 +85,30 @@ void check100(int check, int op)
  */
 int main(int argc, char *argv[])
 {
-	int op_from, op_to, close_to, close_from;
+	int fd_from, fd_to, close_to, close_from;
 	ssize_t lenr, lenw;
 	char buffer[1024];
 	mode_t file_perm;
 
 	check97(argc);
-	op_from = open(argv[1], O_RDONLY);
-	check98((ssize_t)op_from, argv[1], -1, -1);
+	fd_from = open(argv[1], O_RDONLY);
+	check98((ssize_t)fd_from, argv[1], -1, -1);
 	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	op_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
-	check99((ssize_t)op_to, argv[2], op_from, -1);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
+	check99((ssize_t)fd_to, argv[2], fd_from, -1);
 	lenr = 1024;
 	while (lenr == 1024)
 	{
-		lenr = read(op_from, buffer, 1024);
-		check98(lenr, argv[1], op_from, op_to);
-		lenw = write(op_to, buffer, lenr);
+		lenr = read(fd_from, buffer, 1024);
+		check98(lenr, argv[1], fd_from, fd_to);
+		lenw = write(fd_to, buffer, lenr);
 		if (lenw != lenr)
 			lenw = -1;
-		check99(lenw, argv[2], op_from, op_to);
+		check99(lenw, argv[2], fd_from, fd_to);
 	}
-	close_to = close(op_to);
-	close_from = close(op_from);
-	check100(close_to, op_to);
-	check100(close_from, op_from);
+	close_to = close(fd_to);
+	close_from = close(fd_from);
+	check100(close_to, fd_to);
+	check100(close_from, fd_from);
 	return (0);
 }
