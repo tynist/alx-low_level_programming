e911176f580d0ed149fd19c6114d3f745a3d571f
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-10-20 23:52:19 +0100

    commits

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,21 +8,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i = 0;
-	hash_table_t *new_ht = malloc(sizeof(hash_table_t));
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 
-	if (!new_ht)
+	if (!new_table)
 		return (NULL);
 
-	new_ht->size = size;
+	new_table->size = size;
 
-	new_ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (!new_ht->array)
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!new_table->array)
 	{
-		free(new_ht);
+		free(new_table);
 		return (NULL);
 	}
 
 	for (; i < size; i++)
-		new_ht->array[i] = NULL;
-	return (new_ht);
+		new_table->array[i] = NULL;
+
+	return (new_table);
 }
