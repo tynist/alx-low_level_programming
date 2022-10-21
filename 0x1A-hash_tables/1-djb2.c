89969cde515f79da2ab4468b9ac80853052e18ba
tynist <ximonaka@gmail.com>
2022-10-21 03:43:02 -0700

    commits

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -3,7 +3,6 @@
 /**
  * hash_djb2 - implementation of the djb2 algorithm
  * @str: string used to generate hash value
- * 
  * Return: hash value
  */
 unsigned long int hash_djb2(const unsigned char *str)
