201572117fd4c7745dfb723fb42ec23ada33f72c
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-10-21 21:37:12 +0100

    commits

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,10 +1,8 @@
-#ifndef _HASH_TABLES_H
-#define _HASH_TABLES_H
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-#include <stdbool.h>
 #include <string.h>
 
 /**
@@ -17,9 +15,9 @@
  */
 typedef struct hash_node_s
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
+     char *key;
+     char *value;
+     struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -32,8 +30,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-	unsigned long int size;
-	hash_node_t **array;
+     unsigned long int size;
+     hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
@@ -56,11 +54,11 @@ void hash_table_delete(hash_table_t *ht);
  */
 typedef struct shash_node_s
 {
-	char *key;
-	char *value;
-	struct shash_node_s *next;
-	struct shash_node_s *sprev;
-	struct shash_node_s *snext;
+     char *key;
+     char *value;
+     struct shash_node_s *next;
+     struct shash_node_s *sprev;
+     struct shash_node_s *snext;
 } shash_node_t;
 
 /**
@@ -75,10 +73,10 @@ typedef struct shash_node_s
  */
 typedef struct shash_table_s
 {
-	unsigned long int size;
-	shash_node_t **array;
-	shash_node_t *shead;
-	shash_node_t *stail;
+     unsigned long int size;
+     shash_node_t **array;
+     shash_node_t *shead;
+     shash_node_t *stail;
 } shash_table_t;
 
 shash_table_t *shash_table_create(unsigned long int size);
@@ -88,4 +86,4 @@ void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 
-#endif /*_HASH_TABLES_ */
+#endif /* HASH_TABLES */
