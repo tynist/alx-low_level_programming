b0f5de6abe077877e30cc4bf64f02cd3b0e2b57f
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-10-21 05:23:47 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,6 +3,7 @@
 /**
  * hash_table_create - Create a new hash table
  * @size: Size of the array
+ *
  * Return: Pointer to the new hash table,
  */
 hash_table_t *hash_table_create(unsigned long int size)
