768c3946ab28083aa8fad5152ccb9e32acd984c0
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-01-28 12:13:12 +0100

    103-exponential

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,23 +1,16 @@
-/*
- * File: 103-exponential.c
- * Auth: Brennan D Baraban
- */
-
 #include "search_algos.h"
 
 /**
-  * _binary_search - Searches for a value in a sorted array
-  *                  of integers using binary search.
-  * @array: A pointer to the first element of the array to search.
-  * @left: The starting index of the [sub]array to search.
-  * @right: The ending index of the [sub]array to search.
-  * @value: The value to search for.
-  *
-  * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the index where the value is located.
-  *
-  * Description: Prints the [sub]array being searched after each change.
-  */
+ * _binary_search - Searches for a value in a sorted array
+ *                  of integers using binary search.
+ * @array: pointer to the first element of the array to search.
+ * @left: The starting index of the [sub]array to search.
+ * @right: The ending index of the [sub]array to search.
+ * @value: The value to search for.
+ * Return: the index where the value is located,
+ *         Otherwise -1 If the value is not present or the array is NULL
+ * Description: Prints the [sub]array being searched after each change.
+ */
 int _binary_search(int *array, size_t left, size_t right, int value)
 {
 	size_t i;
@@ -50,10 +43,8 @@ int _binary_search(int *array, size_t left, size_t right, int value)
   * @array: A pointer to the first element of the array to search.
   * @size: The number of elements in the array.
   * @value: The value to search for.
-  *
-  * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the index where the value is located.
-  *
+  * Return: the index where the value is located,
+  *         Otherwise -1 If the value is not present or the array is NULL
   * Description: Prints a value every time it is compared in the array.
   */
 int exponential_search(int *array, size_t size, int value)
