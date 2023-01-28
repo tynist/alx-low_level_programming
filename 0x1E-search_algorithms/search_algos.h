86ecc4bfd36c4994db7cf6ac245781a77fc50653
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-01-28 12:08:13 +0100

    search_algos

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,13 +7,10 @@
 
 /**
  * struct listint_s - singly linked list
- *
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
- *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct listint_s
 {
@@ -22,16 +19,13 @@ typedef struct listint_s
 		struct listint_s *next;
 } listint_t;
 
+
 /**
  * struct skiplist_s - Singly linked list with an express lane
- *
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
  * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
- * for Holberton project
  */
 typedef struct skiplist_s
 {
