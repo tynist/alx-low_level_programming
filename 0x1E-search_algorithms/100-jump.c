69a0bc4d39e5e0878446d80cea340ac6224db6d4
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-01-28 11:25:05 +0100

    100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,23 +1,16 @@
-/*
- * File: 100-jump.c
- * Auth: Brennan D Baraban
- */
-
+#include <math.h>
 #include "search_algos.h"
 
 /**
-  * jump_search - Searches for a value in a sorted array
-  *               of integers using jump search.
-  * @array: A pointer to the first element of the array to search.
-  * @size: The number of elements in the array.
-  * @value: The value to search for.
-  *
-  * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the first index where the value is located.
-  *
-  * Description: Prints a value every time it is compared in the array.
-  *              Uses the square root of the array size as the jump step.
-  */
+ * jump_search - Searches for a value in a sorted array
+ *               of integers using jump search.
+ * @array: A pointer to the first element of the array to search.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ * Return: The first index where the value is located,
+ *         Otherwise -1, if the value is not present or the array is NULL.
+ */
+
 int jump_search(int *array, size_t size, int value)
 {
 	size_t i, jump, step;
